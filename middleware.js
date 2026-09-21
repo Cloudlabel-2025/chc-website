f065f8edbe74507aa76f6706bc842241a5bffba4
karun <skygazerk@gmail.com>
2026-09-21 23:04:08 +0530

    login-bypassFix

--- middleware.js
+++ middleware.js
@@ -63,5 +63,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/admin/:path*', '/api/admin/:path*'],
+  matcher: ['/admin', '/admin/:path*', '/api/admin/:path*'],
 }
